4a94efd26a5bd17fbe534d6b6a0c8a714ca841e9
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-04-29 07:49:16 +0100

    Create a store, an action and a reducer

--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,13 +1,20 @@
-import { createStore } from "redux";
-// import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 const initialState = {
-  things: []
+  things: [
+    {
+      name: "test",
+      guild: "123"  
+    }
+  ]
 };
 
 function rootReducer(state, action) {
   console.log(action.type);
   switch (action.type) {
+    case "GET_THINGS_SUCCESS":
+      return { things: action.json.things };
     default:
       return state
   };
@@ -17,6 +24,7 @@ export default function configureStore() {
   const store = createStore(
     rootReducer,
     initialState,
+    applyMiddleware(thunk)
     );
   return store;
 }
