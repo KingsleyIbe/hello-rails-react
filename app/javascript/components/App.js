a236519f2cb72a66cfda8a5da9ff38ba0b5531fe
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-04-29 07:21:23 +0100

    Use rounter

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,18 +1,18 @@
 import React from "react"
-import PropTypes from "prop-types"
-import { BrowserRouter, Route, Switch  } from "react-router-dom";
+// import PropTypes from "prop-types"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HelloWorld from './HelloWorld';
 
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-      <Switch>
-        <Route path="/" render={() => ("Home!")}/>
-        <Route path="/hello" render={() => <HelloWorld greeting="Friend" />}/>
-      </Switch>
-    </BrowserRouter>
+    <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HelloWorld/>} />
+              <Route path="/hello" element={<HelloWorld />} />
+            </Routes>
+          </BrowserRouter>
     );
   }
 }
