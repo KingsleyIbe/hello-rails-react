ff5fb21924b3ac367baa26413b0566963c04ae6f
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-04-29 08:05:01 +0100

    Fix redux issues

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 // import PropTypes from "prop-types"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import configureStore from '../configureStore';
 import { Provider } from "react-redux";
 
 import HelloWorld from "./HelloWorld";
 import Home from "./Home";
 import Greeting from "./Greeting";
+
+const store = configureStore();
 
 class App extends React.Component {
   render() {
